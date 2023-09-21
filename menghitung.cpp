91f646ca2170a87b570cfc720b9e1dd9d0ddf474
nurrfhelizia <144641820+nurrfhelizia@users.noreply.github.com>
2023-09-21 16:17:37 +0700

    Create menghitung.cpp

---
+++ menghitung.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cout<<"masukkan angka pertama:";
+    cin>>a;
+    cout<<"masukkan angka kedua:";
+    cin>>b;
+    c=a+b;
+    cout<<"hasilnya adalah:"<<c<<endl;
+    return 0;}
